6187d201cf1e3e977c6d2ec177de1ea03af17e47
Ashley <thesurfacelog@gmail.com>
2026-01-22 08:12:05 -0500

    Initial Next.js setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-green-400 flex items-center justify-center">
+      <h1 className="text-4xl font-mono">
+        THE SURFACE LOG
+      </h1>
+    </main>
+  )
+}
